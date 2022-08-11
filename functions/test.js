81c2d48b2ef8eb801df38678538d37e1a288aa3c
Blazze23 <l.azmar23@gmail.com>
2022-08-11 18:37:25 +0200

    updated esbundler

--- functions/test.js
+++ functions/test.js
@@ -1,21 +1,20 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../fir-chat-d3c37-firebase-adminsdk-a02bn-43620c440d.json");
+import { admin } from "firebase-admin";
+import { serviceAccount } from "../fir-chat-d3c37-firebase-adminsdk-a02bn-43620c440d.json";
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://firebase-chat.firebaseio.com",
 });
 
-getAuth()
-  .getUser(uid)
-  .then((userRecord) => {
-    // See the UserRecord reference doc for the contents of userRecord.
-    console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
-  })
-  .catch((error) => {
-    console.log("Error fetching user data:", error);
-  });
+// getAuth()
+//   .getUser(uid)
+//   .then((userRecord) => {
+//     // See the UserRecord reference doc for the contents of userRecord.
+//     console.log(`Successfully fetched user data: ${userRecord.toJSON()}`);
+//   })
+//   .catch((error) => {
+//     console.log("Error fetching user data:", error);
+//   });
 
 exports.handler = (event, context, callback) => {
   callback(null, {
